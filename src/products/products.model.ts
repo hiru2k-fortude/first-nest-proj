22f775780fdd0d84dcef3c701f87e65d9fd5681b
hiru2k-fortude <HiruMu@fortude.co>
2024-05-11 18:15:52 +0530

    created product entity

---
+++ src/products/products.model.ts
@@ -0,0 +1,10 @@
+//model defines the structure and behavior of data within the applciation
+
+export class Product{
+   
+
+    constructor(public id:string,public title:string,public description:string,public price:number){
+
+
+    }
+}
